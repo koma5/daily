82cf1663729d5132c96aa6c902d1c392fa31419f
koma5 <marco@5th.ch>
2011-12-22 16:16:22 +0100

    nicer variable names

--- daylength.php
+++ daylength.php
@@ -12,15 +12,19 @@
                         \_          cos(L*pi/180)*cos(P)           _/
 */
 
-$J = date("z");
-$L = 47.2167; //Rapperswil
+$dayofyear = date("z");
+$longitude = 47.2167; //Rapperswil
 
 
-$P = asin(0.39795*cos(0.2163108 + 2*atan(0.9671396*tan(0.00860*($J-186)))));
-$D = 24 - (24/pi())*acos(    (sin(0.8333*pi()/180) + sin($L*pi()/180)*sin($P))   /   (cos($L*pi()/180)*cos($P))    );
+$P = asin(0.39795*cos(0.2163108 + 2*atan(0.9671396*tan(0.00860*($dayofyear-186)))));
+$daylength = 24 - (24/pi())*acos(    (sin(0.8333*pi()/180) + sin($longitude*pi()/180)*sin($P))   /   (cos($longitude*pi()/180)*cos($P))    );
 
-echo "<h1>Rapperswil: </h1><small>on day ". $J ."</small><br>";
-echo "day length: " . $D . "<br>";
-echo "sunrise at: " . (12-$D/2) . "<br>";
-echo "sunset at: " . (12+$D/2) . "<br>";
+$sunTransitTime = 12.389166666666666; //would have expectet midday o.O
+$sunriseTime = $sunTransitTime-$daylength/2;
+$sunsetTime = $sunTransitTime+$daylength/2;
+
+echo "<h1>Rapperswil: </h1><small>on day " . $dayofyear . "</small><br>";
+echo "day length: " . $daylength . "<br>";
+echo "sunrise at: " . $sunriseTime . "<br>";
+echo "sunset at: " . $sunsetTime . "<br>";
 ?>
