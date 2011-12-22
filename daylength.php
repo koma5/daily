73d7dddf9242334e2fa191eeed387c82312999ed
koma5 <marco@5th.ch>
2011-12-22 16:21:08 +0100

    fixed typo

--- daylength.php
+++ daylength.php
@@ -12,18 +12,18 @@
                         \_          cos(L*pi/180)*cos(P)           _/
 */
 
-$dayofyear = date("z");
+$dayofyear = date("z"); //current day of year
 $longitude = 47.2167; //Rapperswil
 
 
 $P = asin(0.39795*cos(0.2163108 + 2*atan(0.9671396*tan(0.00860*($dayofyear-186)))));
 $daylength = 24 - (24/pi())*acos(    (sin(0.8333*pi()/180) + sin($longitude*pi()/180)*sin($P))   /   (cos($longitude*pi()/180)*cos($P))    );
 
-$sunTransitTime = 12.389166666666666; //would have expectet midday o.O
+$sunTransitTime = 12.389166666666666; //would have expected midday 12 == o'clock o.O
 $sunriseTime = $sunTransitTime-$daylength/2;
 $sunsetTime = $sunTransitTime+$daylength/2;
 
-echo "<h1>Rapperswil: </h1><small>on day " . $dayofyear . "</small><br>";
+echo "<h1>Rapperswil: </h1>on day " . $dayofyear . "<br>";
 echo "day length: " . $daylength . "<br>";
 echo "sunrise at: " . $sunriseTime . "<br>";
 echo "sunset at: " . $sunsetTime . "<br>";
