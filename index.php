d7e2a234b33e1a29ca65576a6834b2f84917eeff
marco <marco@5th.ch>
2011-12-29 19:38:43 +0100

    added ol ul dummy data

--- index.php
+++ index.php
@@ -13,9 +13,10 @@
   
   </head>
   
-  <body>
+  <body id="home">
   
     <div class="container">
+    
       <header>
         <span class="row">
           <span class="span4 offset12">
@@ -27,55 +28,72 @@
         </span>
     
       </header>
+      
       <article class="post">
     
           <h1>
-          blog.5th.ch<span class="path"><a href="#">/wie-bist-du-hier-gelandet</a></span>
+          <a id="home" href="#" rel="home">blog.5th.ch</a><span class="path"><a href="#">/wie-bist-du-hier-gelandet</a></span>
           <small>koma5, 4.12.'11 tags: blahh, bubb</small>
           </h1>
           
-<p>Oft ist eine Webseite über mehrere URLs erreichbar. Als Beispiel dient dieser Blog hier:
-<ul>
-	<li>blog.5th.ch/</li>
-	<li>www.blog.5th.ch/</li>
-	<li>5th.ch/blog/</li>
-	<li>www.5th.ch/blog/</li>
-	<li>fifth.ch/blog/</li>
-	<li>usw.</li>
-</ul>
-</p>
-<p>Das sind fünf Wege, die zu diesem Blog führen. Fünf völlig verschiedene Links mit ein und demselben Inhalt. Einerseits sieht das überhaupt nicht schön aus und andererseits entsteht so ein Link-Wildwuchs. Deshalb ist es empfehlenswert, eine canonical URL zu definieren. Eine einzige URL, unter welcher die Webseite erscheinen soll. Alle anderen werden darauf umgeleitet.</p>
-
-<p>Das <code>.htaccess</code> File ist eine ganz nützliche Sache. Es enthält Apache Konfigurationen pro Verzeichnis, mit welchem ganz leicht Weiterleitungen definiert werden können.  In diesem Sinne habe ich der <code>.htaccess</code> dieses Blogs folgende Zeilen hinzugefügt:</p>
-
-<p><code>#redirect everything to canonical URL exept the canonical URL itself...
-RewriteEngine On<br>
-RewriteCond %{HTTP_HOST} !^blog.5th.ch$<br>
-RewriteRule ^(.*)$ http://blog.5th.ch/$1 [R=301,L]</code></p>
-
-<p>Was soviel Bedeutet wie:<br>
-<code>hostname != blog.5th.ch<br>
-redirect to blog.5th.ch (canonical URL)</code></p>
-
-<p>Wenn der aufgerufene Hostname nicht die canonical URL ist, dann leite die Anfrage an die canonical URL um. Gleichzeitig wird der Pfad der ursprünglichen URL an die canonical URL gehängt. Das heisst, aus dem
-<code><a href="http://www.blog.5th.ch/htaccess-redirect-everything-to-canonical-url">www.blog.5th.ch/htaccess-redirect-everything-to-canonical-url
-</a></code>wird <code><a href="http://blog.5th.ch/htaccess-redirect-everything-to-canonical-url">blog.5th.ch/htaccess-redirect-everything-to-canonical-url</a></code> .</p>
-
-<p>Die Weiterleitung wird allerdings auf Unterordner vererbt, was ziemlich störend sein kann.</p>
-
-<h1>heading 1<small>small text</small></h1>
-<h2>heading 2<small>small text</small></h2>
-<h3>heading 3<small>small text</small></h3>
-<h4>heading 4<small>small text</small></h4>
-<h5>heading 5<small>small text</small></h5>
-<h6>heading 6<small>small text</small></h6>
+          <p>Oft ist eine Webseite über mehrere URLs erreichbar. Als Beispiel dient dieser Blog hier:</p>
+          
+          ul
+          <ul>
+          	<li>blog.5th.ch/</li>
+          	<li>www.blog.5th.ch/</li>
+          	<li>5th.ch/blog/</li>
+          	<li>www.5th.ch/blog/</li>
+          	<li>fifth.ch/blog/</li>
+          	<li>usw.</li>
+          </ul>
+          <br>
+          ol
+          <ol>
+          	<li>blog.5th.ch/</li>
+          	<li>www.blog.5th.ch/</li>
+          	<li>5th.ch/blog/</li>
+          	<li>www.5th.ch/blog/</li>
+          	<li>fifth.ch/blog/</li>
+          	<li>usw.</li>
+          </ol>
+          
+          
+          <p>Das sind fünf Wege, die zu diesem Blog führen. Fünf völlig verschiedene Links mit ein und demselben Inhalt. Einerseits sieht das überhaupt nicht schön aus und andererseits entsteht so ein Link-Wildwuchs. Deshalb ist es empfehlenswert, eine canonical URL zu definieren. Eine einzige URL, unter welcher die Webseite erscheinen soll. Alle anderen werden darauf umgeleitet.</p>
+          
+          <p>Das <code>.htaccess</code> File ist eine ganz nützliche Sache. Es enthält Apache Konfigurationen pro Verzeichnis, mit welchem ganz leicht Weiterleitungen definiert werden können.  In diesem Sinne habe ich der <code>.htaccess</code> dieses Blogs folgende Zeilen hinzugefügt:</p>
+          
+          <p><code>#redirect everything to canonical URL exept the canonical URL itself...
+          RewriteEngine On<br>
+          RewriteCond %{HTTP_HOST} !^blog.5th.ch$<br>
+          RewriteRule ^(.*)$ http://blog.5th.ch/$1 [R=301,L]</code></p>
+          
+          <p>Was soviel Bedeutet wie:<br>
+          <code>hostname != blog.5th.ch<br>
+          redirect to blog.5th.ch (canonical URL)</code></p>
+          
+          <p>Wenn der aufgerufene Hostname nicht die canonical URL ist, dann leite die Anfrage an die canonical URL um. Gleichzeitig wird der Pfad der ursprünglichen URL an die canonical URL gehängt. Das heisst, aus dem
+          <code><a href="http://www.blog.5th.ch/htaccess-redirect-everything-to-canonical-url">www.blog.5th.ch/htaccess-redirect-everything-to-canonical-url
+          </a></code>wird <code><a href="http://blog.5th.ch/htaccess-redirect-everything-to-canonical-url">blog.5th.ch/htaccess-redirect-everything-to-canonical-url</a></code> .</p>
+          
+          <p>Die Weiterleitung wird allerdings auf Unterordner vererbt, was ziemlich störend sein kann.</p>
+          
+          <pre>hier ist ein pre-Tag</pre>
+          
+          <h1>heading 1<small>small text</small></h1>
+          <h2>heading 2<small>small text</small></h2>
+          <h3>heading 3<small>small text</small></h3>
+          <h4>heading 4<small>small text</small></h4>
+          <h5>heading 5<small>small text</small></h5>
+          <h6>heading 6<small>small text</small></h6>
         </article>
     
     
       <footer>
-        <p>by koma5</p>
+        <p>© copyright koma5 - Proudly powerd by WordPress - theme daily</p>
       </footer>
     
     </div>
+    <!-- /container -->
   </body>
 </html>
