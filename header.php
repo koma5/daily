75226c1ba009ee472bc804cd9dea6671c2630064
koma5 <marco@5th.ch>
2012-01-07 01:03:33 +0100

    changed nav

--- header.php
+++ header.php
@@ -6,7 +6,6 @@
   <title><?php bloginfo('name'); the_title(); ?></title>
   
   <link rel="stylesheet" type="text/css" href="<?php bloginfo( 'stylesheet_url' ); ?>">
-  <!-- <link rel="stylesheet" href="http://twitter.github.com/bootstrap/1.4.0/bootstrap.min.css">-->
   <style type="text/css">
   
   </style>
@@ -18,13 +17,12 @@
     <div class="container">
     
       <header>
-        <span class="row">
-          <span class="span4 offset12">
-            <ul class="nav">
-              <li><a href="">rss</a></li>
-              <li><a href="#">impressum</a></li>
-            </ul>
-          </span>
-        </span>
+      
+            <nav>
+              <ul>
+                <li><a href="<?php bloginfo('rss2_url'); ?>">rss</a></li>
+                <li><a href="#">impressum</a></li>
+              </ul>
+            </nav>
     
       </header>
