582acee6c2837a26c514c3c81f09cf7eda09b6d9
marco <marco@5th.ch>
2012-01-28 19:32:37 +0100

    footer proudly powerd by WordPress, cyon, theme daily and me =P

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
       <footer>
-        <p>© copyright koma5 - Proudly powerd by WordPress - theme daily</p>
+        <p>Proudly powerd by WordPress, <a href="http://cyon.ch">cyon</a>, <a href="http://blog.5th.ch/wordpress-theme-daily">theme daily</a> and <a href="http://twitter.com/koma5">koma5</a></p>
       </footer>
     
     </div>
