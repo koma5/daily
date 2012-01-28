5ba85bc49c1939321ad35f00b4255d4a9687ac49
marco <marco@5th.ch>
2012-01-28 20:35:35 +0100

    nicer color in page/entry title by navigating to a single post =D

--- single.php
+++ single.php
@@ -5,7 +5,7 @@
 
       <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
   
-      <article class="entry" id="<?php echo basename(get_permalink()); ?>">
+      <article class="entry-single" id="<?php echo basename(get_permalink()); ?>">
         <h1 class="title">
           <a class="home" rel="home" href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a><a class="path" rel="bookmark" href="<?php the_permalink(); ?>">/<?php the_title();?></a>
           <small class="meta">
