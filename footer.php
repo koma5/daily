053db1c3eef722bf03b5bc876a2a77282d666402
marco <marco@5th.ch>
2012-02-05 01:15:30 +0100

    WordPress Link

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
       <footer>
-        <p>Proudly powerd by WordPress, <a href="http://cyon.ch">cyon</a>, <a href="http://blog.5th.ch/wordpress-theme-daily">theme daily</a> and <a href="http://twitter.com/koma5">koma5</a></p>
+        <p>Proudly powerd by <a href="http://wordpress.org">WordPress</a>, <a href="http://cyon.ch">cyon</a>, <a href="http://blog.5th.ch/wordpress-theme-daily">theme daily</a> and <a href="http://twitter.com/koma5">koma5</a></p>
       </footer>
     
     </div>
